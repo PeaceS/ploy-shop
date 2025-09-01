89719e22032c8d9b5b5454c6d280c3ea947d18ca
PeaceS <p.saravudecha@gmail.com>
2025-09-01 13:43:03 +0700

    feat: try onApprove

--- public/paypal.js
+++ public/paypal.js
@@ -34,5 +34,14 @@ paypal.Buttons({
   },
   createOrder: function(_data, actions) {
     return createOrder('50.00', actions);
+  },
+  onApprove: function(_data, actions) {
+    // Here you would capture the order on your server.
+    // For this example, we'll just show a success message.
+    return actions.order.capture().then(function(orderData) {
+        console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+        const transaction = orderData.purchase_units[0].payments.captures[0];
+        alert('Transaction ' + transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+    });
   }
 }).render('#paypal-button-container-2');
