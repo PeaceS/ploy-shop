95e06e22b328b496586a02c64580a6ed00d081c4
PeaceS <p.saravudecha@gmail.com>
2025-09-01 13:57:33 +0700

    feat: redirect after paid

--- public/paypal.js
+++ public/paypal.js
@@ -36,12 +36,11 @@ paypal.Buttons({
     return createOrder('50.00', actions);
   },
   onApprove: function(_data, actions) {
-    // Here you would capture the order on your server.
-    // For this example, we'll just show a success message.
     return actions.order.capture().then(function(orderData) {
-        console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-        const transaction = orderData.purchase_units[0].payments.captures[0];
-        alert('Transaction ' + transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+        console.log(orderData.payer.name.given_name);
+        console.log(orderData.payer.name.surname);
+        console.log(orderData.payer.email_address);
+        window.location.href = '/thank-you';
     });
   }
 }).render('#paypal-button-container-2');
