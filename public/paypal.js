09764968112d156b54c26ef3d600e33a3c0c4f1d
PeaceS <p.saravudecha@gmail.com>
2025-09-01 12:59:06 +0700

    fix: try only one button

--- public/paypal.js
+++ public/paypal.js
@@ -20,26 +20,3 @@ paypal.Buttons({
     });
   }
 }).render('#paypal-button-container-1');
-
-paypal.Buttons({
-  style: {
-    layout: 'vertical',
-    color:  'blue',
-    shape:  'rect',
-    label:  'paypal'
-  },
-  createOrder: function(_data, actions) {
-    return actions.order.create({
-      purchase_units: [{
-        amount: {
-          value: '40.00'
-        }
-      }],
-      application_context: {
-        brand_name: 'Rungploy',
-        locale: 'de-DE',
-        shipping_preference: 'NO_SHIPPING'
-      }
-    });
-  }
-}).render('#paypal-button-container-2');
