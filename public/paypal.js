e800f2bdde379f331c27aa5867752015125270ed
PeaceS <p.saravudecha@gmail.com>
2025-09-01 12:27:30 +0700

    fix: ui + js a bit

--- public/paypal.js
+++ public/paypal.js
@@ -7,9 +7,11 @@ paypal.Buttons({
   },
   createOrder: function(_data, actions) {
     return actions.order.create({
-      brand_name: 'Rungploy',
-      locale: 'de-DE',
-      shipping_preference: 'NO_SHIPPING',
+      application_context: [{
+        brand_name: 'Rungploy',
+        locale: 'de-DE',
+        shipping_preference: 'NO_SHIPPING'
+      }],
       purchase_units: [{
         amount: {
           value: '40.00'
