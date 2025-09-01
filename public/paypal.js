025d4d5e188b20b32a566552b8269b9d4fd70b54
PeaceS <p.saravudecha@gmail.com>
2025-09-01 13:10:39 +0700

    fix: try to apply on both buttons

--- public/paypal.js
+++ public/paypal.js
@@ -1,3 +1,18 @@
+function createOrder(amount, actions) {
+  return actions.order.create({
+    purchase_units: [{
+      amount: {
+        value: amount
+      }
+    }],
+    application_context: {
+      brand_name: 'Rungploy',
+      locale: 'de-DE',
+      shipping_preference: 'NO_SHIPPING'
+    }
+  });
+}
+
 paypal.Buttons({
   style: {
     layout: 'vertical',
@@ -6,17 +21,18 @@ paypal.Buttons({
     label:  'paypal'
   },
   createOrder: function(_data, actions) {
-    return actions.order.create({
-      purchase_units: [{
-        amount: {
-          value: '40.00'
-        }
-      }],
-      application_context: {
-        brand_name: 'Rungploy',
-        locale: 'de-DE',
-        shipping_preference: 'NO_SHIPPING'
-      }
-    });
+    return createOrder('40.00', actions);
   }
 }).render('#paypal-button-container-1');
+
+paypal.Buttons({
+  style: {
+    layout: 'vertical',
+    color:  'blue',
+    shape:  'rect',
+    label:  'paypal'
+  },
+  createOrder: function(_data, actions) {
+    return createOrder('50.00', actions);
+  }
+}).render('#paypal-button-container-2');
