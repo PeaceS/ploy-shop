0529ee57048dff91d05344fb86a246f7dc4efd5e
PeaceS <p.saravudecha@gmail.com>
2025-09-18 15:38:23 +0700

    feat: add stock list for keychain

--- public/main.js
+++ public/main.js
@@ -1,70 +1,23 @@
-async function fetchProducts() {
+async function fetchKeychainStock() {
   try {
-    const response = await fetch('/products');
+    const response = await fetch('/keychains');
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    const products = await response.json();
-    const productContainer = document.getElementById('product-container');
-    const productTemplate = document.getElementById('product-template');
+    const keychains = await response.json();
+    const stockContainer = document.getElementById('stock');
 
-    products.forEach(product => {
-      const productDiv = productTemplate.cloneNode(true);
-      productDiv.removeAttribute('id');
-
-      titleDiv = productDiv.querySelector('.product-title');
-      titleDiv.textContent = product.item;
-
-      imageDiv = productDiv.querySelector('.product-image');
-      imageDiv.src = `${product.image_directory}/1.jpg`;
-      imageDiv.alt = product.item;
-      imageDiv.id = product.id
-
-      descriptionDiv = productDiv.querySelector('.product-description');
-      descriptionDiv.textContent = product.description;
-
-      priceDiv = productDiv.querySelector('.product-price');
-      priceDiv.textContent = product.price;
-
-      let currentImageIndex = 0;
-      const images = [
-        `${product.image_directory}/1.jpg`,
-        `${product.image_directory}/2.jpg`
-      ];
-
-      const updateImage = (id) => {
-        imageDiv = document.getElementById(id);
-        imageDiv.classList.remove('show');
-
-        // Wait for the fade-out to complete before changing the image source
-        setTimeout(() => {
-            imageDiv.src = images[currentImageIndex];
-            // Fade in the new image
-            imageDiv.classList.add('show');
-        }, 500); // This delay should match the CSS transition duration
-      };
-
-      prevBtn = productDiv.querySelector('.previous-btn');
-      prevBtn.addEventListener('click', () => {
-        currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-        updateImage(product.id);
-      });
-
-      nextBtn = productDiv.querySelector('.next-btn');
-      nextBtn.addEventListener('click', () => {
-        currentImageIndex = (currentImageIndex + 1) % images.length;
-        updateImage(product.id);
-      });
-
-      productContainer.appendChild(productDiv);
+    keychains.forEach(keychain => {
+      const li = document.createElement('li');
+      li.textContent = `${keychain.item} - ${keychain.stock}`;
+      stockContainer.appendChild(li);
     });
 
-    productTemplate.classList.add('hide');
   } catch (error) {
     console.error('Failed to fetch products:', error);
   }
 }
 
-fetchProducts();
+fetchKeychainStock();
