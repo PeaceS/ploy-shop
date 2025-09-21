76682d0026251abba502897612f459994752e5f5
PeaceS <p.saravudecha@gmail.com>
2025-09-21 16:01:52 +0700

    feat: add the bond container

--- public/main.js
+++ public/main.js
@@ -1,6 +1,13 @@
-async function soldFn(id) {
+function formatId(id) {
+  const idAsString = String(id);
+  const paddedId = idAsString.padStart(3, '0');
+
+  return `TB-${paddedId}`;
+}
+
+async function soldFn(item, id) {
   try {
-    const response = await fetch(`/keychains/${id}`, {
+    const response = await fetch(`/${item}/${id}`, {
       method: 'PUT',
     });
 
@@ -47,15 +54,59 @@ async function fetchKeychainStock() {
       row.appendChild(sold);
 
       soldButton.addEventListener('click', () => {
-        soldFn(keychain.id);
+        soldFn('keychains', keychain.id);
       });
 
       keychainStockContainer.appendChild(row);
     });
 
   } catch (error) {
-    console.error('Failed to fetch products:', error);
+    console.error('Failed to fetch keychains:', error);
+  }
+}
+
+async function fetchTheBondStock(id) {
+  try {
+    endpoint = id ? `/bonds/${id}` : '/bonds';
+    const response = await fetch(endpoint);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const theBonds = await response.json();
+    const bondStockContainer = document.getElementById('bond');
+    const hiddenSoldButton = document.getElementById('sold-button');
+
+    theBonds.forEach(theBond => {
+      const row = document.createElement('tr');
+
+      const item = document.createElement('td');
+      item.textContent = formatId(theBond.id);
+      row.appendChild(item);
+
+      const sold = document.createElement('td');
+
+      if (!theBond.sold) {
+        const soldButton = hiddenSoldButton.cloneNode(true)
+        soldButton.classList.remove('hide');
+        sold.appendChild(soldButton);
+        row.appendChild(sold);
+
+        soldButton.addEventListener('click', () => {
+          soldFn('bonds', theBond.id);
+        });
+      } else {
+        sold.classList.add('sold');
+      }
+
+      bondStockContainer.appendChild(row);
+    });
+
+  } catch (error) {
+    console.error('Failed to fetch the bonds:', error);
   }
 }
 
 fetchKeychainStock();
+fetchTheBondStock();
