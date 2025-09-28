fc7edb31c61cffc0a9bd9157f298b2a765b1a7bc
PeaceS <p.saravudecha@gmail.com>
2025-09-28 17:41:44 +0700

    feat: add webhook

---
+++ functions/webhook/stripe.js
@@ -0,0 +1,34 @@
+export async function onRequestPost(context) {
+  try {
+    const { env, request } = context;
+
+    const stripe = require('stripe')(env.STRIPE_SECRET_KEY);
+    const endpointSecret = env.STRIPE_ENDPOINT_SECRET;
+    const sig = request.headers.get('stripe-signature');
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case 'checkout.session.completed':
+        const checkoutSessionCompleted = event.data.object;
+        // Then define and call a function to handle the event checkout.session.completed
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    return new Response('ok', { status: 200 });
+  } catch (err) {
+    return new Response(`Error fetching keychains: ${err.message}`, { status: 500 });
+  }
+}
