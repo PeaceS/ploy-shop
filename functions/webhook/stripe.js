ef68ddf9698a057036bb03bfc556e72708c5e295
PeaceS <p.saravudecha@gmail.com>
2025-09-28 19:00:44 +0700

    fix: read email, product

--- functions/webhook/stripe.js
+++ functions/webhook/stripe.js
@@ -18,22 +18,19 @@ export async function onRequestPost(context) {
       return new Response(`Webhook Error: ${err.message}`, { status: 400 });
     }
 
-    console.log(event);
-    // Handle the event
     switch (event.type) {
       case 'checkout.session.completed':
         const session = event.data.object;
-        const email = session.customer_details?.email
+        const email = session.customer_details?.email;
+        const productId = session.metadata?.product_id;
         console.log(session);
         console.log(email);
-        // Then define and call a function to handle the event checkout.session.completed
+        console.log(productId);
         break;
-      // ... handle other event types
       default:
         console.log(`Unhandled event type ${event.type}`);
     }
 
-    // Return a 200 response to acknowledge receipt of the event
     return new Response('ok', { status: 200 });
   } catch (err) {
     console.log(err.message);
