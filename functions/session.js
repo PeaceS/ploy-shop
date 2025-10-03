5214244c95f0e634b0850fd499b86c2452d6de7a
PeaceS <p.saravudecha@gmail.com>
2025-10-03 09:15:14 +0700

    fix: session.js

--- functions/session.js
+++ functions/session.js
@@ -5,7 +5,9 @@ export async function onRequestPost(context) {
     const stripe = new Stripe(env.STRIPE_SECRET_KEY);
 
     try {
-        const { priceId, productId } = await request.json(); 
+        const requestBody = await request.json();
+        const productId = requestBody.productId;
+        let priceId = env.PRICE_ID || requestBody.priceId;
 
         const protocol = 'https';
         const host = request.headers.get('host');
