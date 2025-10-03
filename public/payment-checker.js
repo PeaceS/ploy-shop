01851c591f6c0e9b70fb3bef119f32a0cb0428c0
PeaceS <p.saravudecha@gmail.com>
2025-10-03 11:45:42 +0700

    feat: add payment-checker

---
+++ public/payment-checker.js
@@ -0,0 +1,18 @@
+async function fetchTransaction(uuid) {
+  try {
+    const response = await fetch(`/transactions/${uuid}`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const transaction = await response.json();
+
+  } catch (error) {
+    console.error('Failed to fetch transaction:', error);
+  }
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const uuid = urlParams.get('session_id');
+fetchTransaction(uuid);
