85821134dec28d0baff1d2956ad5beb2447bbdcf
PeaceS <p.saravudecha@gmail.com>
2025-10-03 09:05:35 +0700

    feat: success page

--- functions/session.js
+++ functions/session.js
@@ -16,7 +16,7 @@ export async function onRequestPost(context) {
                 quantity: 1,
             }],
             mode: 'payment',
-            success_url: `${protocol}://${host}/success`,
+            success_url: `${protocol}://${host}/success?session_id={CHECKOUT_SESSION_ID}`,
             cancel_url: `${protocol}://${host}/cancel`,
             metadata: {
                 product_id: productId 
---
+++ migrations/0014_add-keychain-products.sql
@@ -0,0 +1,17 @@
+-- Migration number: 0014 	 2025-10-02T12:29:42.362Z
+
+INSERT INTO keychains (item, stock, price, stripe_price_id, display)
+VALUES 
+('Bubble Memory (3S) - Magenta', 19, 50, '', false),
+('Bubble Memory (3S) - Green', 15, 50, '', false),
+('Bubble Memory (3S) - Blue', 20, 50, '', false),
+('Bubble Memory (3S) - Dark brown', 19, 50, '', false),
+('Bubble Memory (3S) - Red brown', 19, 50, '', false),
+('Bubble Memory (3S) - White', 25, 50, '', false),
+('Bubble Memory (3G) - Magenta', 14, 50, '', false),
+('Bubble Memory (3G) - Green', 9, 50, '', false),
+('Bubble Memory (3G) - Blue', 10, 50, '', false),
+('Bubble Memory (3G) - Dark brown', 9, 50, '', false),
+('Bubble Memory (3G) - Red brown', 10, 50, '', false),
+('Bubble Memory (3G) - White', 5, 50, '', false),
+('Bubble Memory (3G) - Nude', 20, 50, '', false);
